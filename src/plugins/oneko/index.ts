198eb51e864e5232cbdd94d3bee45568ea02ccad
alejandroifhegotbuff <alejandroifhegotbuff@gmail.com>
2026-05-17 01:31:04 -0500

    Update index.ts
    this will make it smoother!

--- src/plugins/oneko/index.ts
+++ src/plugins/oneko/index.ts
@@ -27,7 +27,7 @@ export default definePlugin({
     authors: [Devs.Ven, Devs.adryd],
 
     start() {
-        fetch("https://raw.githubusercontent.com/adryd325/oneko.js/c4ee66353b11a44e4a5b7e914a81f8d33111555e/oneko.js")
+        fetch("https://raw.githubusercontent.com/alejandroifhegotbuff/nekocatmyway/refs/heads/main/oneko2.js")
             .then(x => x.text())
             .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
                 .replace("(isReducedMotion)", "(false)"))
